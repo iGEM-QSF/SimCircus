1eb199976f9be1c76871637c556e7844604570d7
Oskari Vinko <oskari.vinko@gmail.com>
2014-07-18 14:07:56 +0300

    Primitive visualization

--- visualize.py
+++ visualize.py
@@ -1,1 +1,31 @@
-import matplotlib
+from matplotlib import pyplot as plt
+from sim import Simulation
+
+
+class Visualization(object):
+    """docstring for Visualization"""
+
+    def __init__(self, simulation):
+        self.simulation = simulation
+        self.colors = ['b', 'g', 'c']
+        pass
+
+    def visualize(self):
+        '''
+        Visualizes the current situation
+        '''
+        for ind, parameter in enumerate(self.simulation.data.keys()):
+            print len(self.simulation.timesteps), len(parameter)
+            plt.plot(
+                self.simulation.timesteps,
+                self.simulation.data.get(parameter),
+                self.colors[ind])
+
+        plt.xlabel("Time")
+        plt.ylabel("Protein concentration")
+        plt.legend(self.simulation.data.keys())
+        plt.show()
+
+sim = Simulation("asd")
+vis = Visualization(sim)
+vis.visualize()
