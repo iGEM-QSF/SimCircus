ded00944f68d8150c47b7a2bfb9f94a113e370ec
Oskari Vinko <oskari.vinko@gmail.com>
2014-07-18 14:08:46 +0300

    Dummydata added to simulation

--- sim.py
+++ sim.py
@@ -1,3 +1,6 @@
+from math import *
+
+
 class Simulation(object):
     """docstring for Simulation"""
 
@@ -6,6 +9,11 @@ class Simulation(object):
         Ask/define the parameters
         '''
         self.arg = arg
+        self.timesteps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        self.data = {
+            "Dummydata": [1, cos(2), 3, 4, 5, 6, 8, 3, 7, 3, 7, 3],
+            "dammydata": [cos(x) for x in self.timesteps]
+        }
         pass
 
     def run(self):
