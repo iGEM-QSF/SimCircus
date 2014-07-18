a4f6d9fa79b57c43a1c0e7c4925bbd882b10866c
Oskari Vinko <oskari.vinko@gmail.com>
2014-07-18 11:17:32 +0300

    Hotfix (Sim.py was in wrong project -.-)

--- sim.py
+++ sim.py
@@ -1,1 +1,27 @@
-print "Hello World!"
+class Simulation(object):
+    """docstring for Simulation"""
+
+    def __init__(self, arg):
+        '''
+        Ask/define the parameters
+        '''
+        self.arg = arg
+        pass
+
+    def run(self):
+        '''
+        Start the simulation
+        '''
+        pass
+
+    def stop(self):
+        '''
+        stop the simulation
+        '''
+        pass
+
+    def cycle(self):
+        '''
+        Iterates through the simultaion steps
+        '''
+        pass
