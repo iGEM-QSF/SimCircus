07d6adfc48a2dad2ed4ec2da87f613d8f890d722
Lamminberg <ottolamminpaa@Otto-MacBook-Pro.local>
2014-07-24 15:02:23 +0300

    New names to variables

--- sim.py
+++ sim.py
@@ -18,21 +18,24 @@ class Simulation(object):
         self.a = 0
         self.b = 0
         self.c = 0
-        self.p1 = 1
-        self.p2 = 0
-        self.p3 = 1
-        self.p4 = 0
-        self.p5 = 1
+        self.promotor1 = 1
+        self.promotor2 = 0
+        self.promotor3 = 1
+        self.promotor4 = 0
+        self.promotor5 = 0
         self.rbs1 = 1
         self.rbs2 = 0.75
-        self.dep1 = 0.5
-        self.dep2 = 0.5
+        self.dePhosCoeff1 = 0.5
+        self.dePhosCoeff2 = 0.5
         self.phosp = 1
         self.ib = 0
-        self.hajYF1 = 1
-        self.hajFixJ = 1
-        self.hajCI = 1
-        self.hajTetR = 1
+        self.degCoeffYF1 = 1
+        self.degCoeffFixJ = 1
+        self.degCoeffCI = 1
+        self.degCoeffTetR = 1
+        self.degCoeffA = 0.5
+        self.degCoeffB = 0.5
+        self.degCoeffC = 0.5
         self.timeStep = 0.1
         self.iterations = 150
         self.data = {
@@ -78,26 +81,35 @@ class Simulation(object):
         return self.data.get(protein)[len(self.data.get(protein)) - 1]
 
     def derivativeYF1(self, protein):
-        return self.p1 * self.rbs1 + self.dep1 * self.getAmount('PYF1') - \
-                (self.hajYF1 + self.ib) * protein
+        return self.promotor1 * self.rbs1 + self.dePhosCoeff1 * self.getAmount('PYF1') - \
+                (self.degCoeffYF1 + self.ib) * protein
 
     def derivativePYF1(self, protein):
         return self.ib * self.getAmount('YF1') - \
-                (self.dep1 + self.hajYF1) * protein
+                (self.dePhosCoeff1 + self.degCoeffYF1) * protein
 
     def derivativeFixJ(self, protein):
-        return self.p1 * self.rbs1 + self.dep2 * self.getAmount('PFixJ') - \
-                (self.phosp * self.getAmount('PYF1') + self.hajFixJ) * protein
+        return self.promotor1 * self.rbs1 + self.dePhosCoeff2 * self.getAmount('PFixJ') - \
+                (self.phosp * self.getAmount('PYF1') + self.degCoeffFixJ) * protein
 
     def derivativePFixJ(self, protein):
         return self.phosp * self.getAmount('FixJ') * self.getAmount('PYF1') - \
-                (self.dep2 + self.hajFixJ) * protein
+                (self.dePhosCoeff2 + self.degCoeffFixJ) * protein
 
     def derivativeCI(self, protein):
-        return self.p2 * self.rbs1 - self.hajCI * protein
+        return self.promotor2 * self.rbs1 - self.degCoeffCI * protein
 
     def derivativeTetR(self, protein):
-        return (self.p3 + self.p4) * self.rbs2 - self.hajTetR * protein
+        return (self.promotor3 + self.promotor4) * self.rbs2 - self.degCoeffTetR * protein
+
+    def derivativeA(self, protein):
+        return self.promotor3 * self.rbs1 - self.degCoeffA * protein
+
+    def derivativeB(self, protein):
+        return self.promotor4 * self.rbs1 - self.degCoeffB * protein
+
+    def derivativeC(self, protein):
+        return self.promotor5 * self.rbs1 - self.degCoeffC * protein
 
     def derivativeSelect(self, name, protein):
         '''
@@ -115,6 +127,18 @@ class Simulation(object):
             return self.derivativeCI(protein)
         elif name == 'TetR':
             return self.derivativeTetR(protein)
+        elif name == 'A':
+            return self.derivativeA(protein)
+        elif name == 'B':
+            return self.derivativeB(protein)
+        elif name == 'C':
+            return self.derivativeC(protein)
+
+    #def promotorUpdate(self):
+    #    self.promotor2 = 5 * self.getAmount('PFixJ')
+    #    self.promotor3 = 1 / (self.getAmount('CI') + 0.0000001)
+    #    self.promotor4 =
+    #    self.promotor5 =
 
     def start(self):
         '''
@@ -122,34 +146,33 @@ class Simulation(object):
         '''
         for i in range(self.iterations):
             for key in self.data:
-                if key == 'A' or key == 'B' or key == 'C':
-                    self.data.get(key).append(0)
-                else:
-                    x = self.getAmount(key)
-                    kerroin1 = self.derivativeSelect(key, x)
-                    kerroin2 = self.derivativeSelect(
-                        key, (x + (kerroin1) * self.timeStep / 2))
-                    kerroin3 = self.derivativeSelect(
-                        key, (x + (kerroin2) * self.timeStep / 2))
-                    kerroin4 = self.derivativeSelect(
-                        key, (x + (kerroin3) * self.timeStep))
-                    self.data.get(key).append(
-                        x + (self.timeStep / 6) * (kerroin1 + 2 * kerroin2 + 2 * kerroin3 + kerroin4))
+                x = self.getAmount(key)
+                coeff1 = self.derivativeSelect(key, x)
+                coeff2 = self.derivativeSelect(
+                    key, (x + (coeff1) * self.timeStep / 2))
+                coeff3 = self.derivativeSelect(
+                    key, (x + (coeff2) * self.timeStep / 2))
+                coeff4 = self.derivativeSelect(
+                    key, (x + (coeff3) * self.timeStep))
+                self.data.get(key).append(
+                    x + (self.timeStep / 6) * (coeff1 + 2 * coeff2 + 2 * coeff3 + coeff4))
             self.timesteps.append(i + 1)
+            #self.promotorUpdate()
             self.visualization.update()
             if i == 50:
-                self.ib = 1
-                self.p2 = 0.5
-                self.p3 = 0
-                self.p4 = 1
+                self.ib = 0.5
+                self.promotor2 = 0.5
+                self.promotor3 = 0
+                self.promotor4 = 1
             if i == 100:
-                self.ib = 2
-                self.p2 = 1
-                self.p4 = 0
+                self.ib = 1
+                self.promotor2 = 1
+                self.promotor4 = 0
+                self.promotor5 = 1
 
-    def testY(self):
-        print self.data
-        print self.timesteps
+#    def testY(self):
+#        print self.data
+#        print self.timesteps
 
 sim = Simulation()
 
